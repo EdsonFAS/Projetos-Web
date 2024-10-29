5cc089ae6da58cdbc591308581127c957fbca20a
EdsonFAS <111073194+EdsonFAS@users.noreply.github.com>
2024-10-29 10:33:14 -0400

    Delete Nova pasta directory

--- Nova pasta/localStorage.js
+++
@@ -1,5 +0,0 @@
-export function salvarCadastro(cadastro) {
-    let cadastros = JSON.parse(localStorage.getItem('cadastros')) || [];
-    cadastros.push(cadastro);
-    localStorage.setItem('cadastros', JSON.stringify(cadastros));
-}
